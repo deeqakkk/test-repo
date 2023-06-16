360943dc8eadeeeb83a9d6f938a3d99c82471e6d
rajneesh44 <rajneeshsharma338@gmail.com>
2023-06-17 00:04:43 +0530

    basic login page

--- app.py
+++ app.py
@@ -4,16 +4,12 @@ load_dotenv()
 import os
 import yaml
 import json
-from flask import Flask, jsonify
+from flask import Flask, request, render_template
 from flask_swagger_ui import get_swaggerui_blueprint
 
 
 app = Flask(__name__)
 
-@app.route('/', methods=["GET"])
-def index():
-    print(os.getenv("PORT"))
-    return "Server is up and running"
 
 def register_blueprints(swagger_blueprint):
     app.register_blueprint(swagger_blueprint)
@@ -30,11 +26,28 @@ def add_swagger_ui():
         }
     )
 
+@app.route('/')
+def login():
+    return render_template('login.html')
+
+@app.route('/test', methods=["GET"])
+def index():
+    return "Server is up and running"
+
 @app.route('/docs')
 def swagger():
     with open('./api-docs.yaml') as f:
         return json.dumps(yaml.safe_load(f))
 
+
+@app.route('/login', methods=['POST'])
+def process_login():
+    email = request.form.get('email')
+    password = request.form.get('password')
+
+    # Process the login data here
+
+    return "Login successful!"
 
 if __name__ == "__main__":
     swagger_blueprint = add_swagger_ui()
